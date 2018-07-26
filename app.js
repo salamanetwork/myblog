64aaa16e9b36b2b31ba01b026d471af35dbe4fd0
Ahmed Mahmoud Salama <ahmed.salama@dewa.gov.ae>
2018-07-26 11:23:23 +0400

    added session, flash messages, validators

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const dn = require('date-time');
 const path = require("path");
 const bp = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+const { check, validationResult } = require('express-validator/check');
+const flash = require('express-flash-notification');
+const session = require('express-session');
 
 var db = require('knex')({
   client: 'mysql',
@@ -26,8 +29,26 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bp.urlencoded({extended: true}));
 app.use(bp.json());
 
+app.use(session({
+  name: 'myBlogNodeJS',
+  secret: 'myBlogNodeJS',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    secure: false,
+    expires: new Date('Monday, 18 January 2028')
+  },
+}));
+
+app.use(flash(app));
+
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
+// Flash message
+app.get('/layout', (req, res) => res.render('layout', {title: "My Blog - Error layout Page"}));
 
 // Home page
 app.get("/", function(req, res) {
@@ -43,6 +64,62 @@ app.get("/", function(req, res) {
 // About page
 app.get("/about", function(req, res) {
   res.render("pages/about", {title: "My Blog - About Page"});
+});
+
+// Register page
+app.get("/register", function(req, res) {
+
+  if (USER_LOGGED === true && USER_ID !== 0) {
+    res.redirect("/");
+  } else {
+    res.render("pages/register", {title: "My Blog - Register A New User Page"});
+  }
+});
+
+app.post("/register", [
+  check('username', "Username must be more the 5 and less than 30 characters").isLength({ min: 5, max: 30 }),
+  check('password', "Password must be more the 5 and less than 30 characters").isLength({ min: 5, max: 30 }),
+  check('email', "Email is not valid").isEmail()
+], (req, res) => {
+  const fullname = req.body.fullname;
+  const username = req.body.username;
+  const email =  req.body.email;
+  const password = bcrypt.hashSync(req.body.password);
+  const description = req.body.description;
+  let joineddate = dn();
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    req.flash('Error', errors.array().map(xErr => "<br />" + xErr['msg'] + " ") , "/layout");
+  } else {
+    db('users').insert({user_name: username, user_password: password, user_email: email, user_full_name: fullname, user_joined_date: joineddate, user_description: description}).then(user => {
+      if (user) {
+        console.log("User Added");
+        res.redirect("/login");
+      } else {
+        console.log("ERROR ADDING USER");
+      }
+    });
+  }
+});
+
+// Profile Page
+app.get("/profile/:id", function(req, res) {
+
+  let uid = req.params.id;
+  let countUserPosts = 0;
+
+  db('users').where({user_id: uid}).select('*').then(rows => {
+    db('posts').where({user_id: uid}).count("*").then( count => {
+      countUserPosts = count[0]['count(*)'];
+      res.render("pages/profile", {
+        rows: rows,
+        countOfPosts: countUserPosts,
+        title: "My Blog - Edit A Post Page"
+      });
+    });
+  });
 });
 
 // Login Page
@@ -140,50 +217,6 @@ app.post("/post/edit", (req, res) => {
   });
 });
 
-// Register page
-app.get("/register", function(req, res) {
 
-  if (USER_LOGGED === true && USER_ID !== 0) {
-    res.redirect("/");
-  } else {
-    res.render("pages/register", {title: "My Blog - Register A New User Page"});
-  }
-});
-
-app.post("/register", (req, res) => {
-  const fullname = req.body.fullname;
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = bcrypt.hashSync(req.body.password);
-  const description = req.body.description;
-  let joineddate = dn();
-
-  db('users').insert({user_name: username, user_password: password, user_email: email, user_full_name: fullname, user_joined_date: joineddate, user_description: description}).then(user => {
-    if (user) {
-      console.log("User Added");
-      res.redirect("/login");
-    } else {
-      console.log("ERROR ADDING USER");
-    }
-  });
-});
-
-// Profile Page
-app.get("/profile/:id", function(req, res) {
-
-  let uid = req.params.id;
-  let countUserPosts = 0;
-
-  db('users').where({user_id: uid}).select('*').then(rows => {
-    db('posts').where({user_id: uid}).count("*").then( count => {
-      countUserPosts = count[0]['count(*)'];
-      res.render("pages/profile", {
-        rows: rows,
-        countOfPosts: countUserPosts,
-        title: "My Blog - Edit A Post Page"
-      });
-    });
-  });
-});
 
 app.listen(3000, console.log("SERVER RUNNING ON PORT: 3000 ON TIME: " + dn()));
